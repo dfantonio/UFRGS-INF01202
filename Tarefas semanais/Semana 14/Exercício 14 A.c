533fa90048e401122fd4176146b835d7d7a51136
Antônio Della Flora <antoniodellaflora@gmail.com>
2020-11-23 16:01:32 -0300

    versão decente do 14A

--- Tarefas semanais/Semana 14/Exercício 14 A.c
+++ Tarefas semanais/Semana 14/Exercício 14 A.c
@@ -2,23 +2,19 @@
 #include <stdlib.h>
 #define QUANT 6
 
-int separador(int num[QUANT], int tamanho, int pares) {
-  if (num[tamanho] % 2 == 0) {
-    pares++;
-  }
-  if (tamanho == 0) {
-    return (pares);
-  }
-  separador(num, tamanho - 1, pares);
+int separador(int num[QUANT], int tamanho) {
+  if (tamanho >= 0)
+    return 1 - num[tamanho] % 2 + separador(num, tamanho - 1);
+
+  return 0;
 }
 
 int main() {
-
-  int num[QUANT], pares = 0;
+  int num[QUANT];
 
   for (int i = 0; i < QUANT; i++) {
     printf("Digite o valor da %dº posição do vetor: ", i + 1);
     scanf("%d", &num[i]);
   }
-  printf("O vetor possui %d números pares.", separador(num, QUANT - 1, pares));
+  printf("O vetor possui %d números pares.", separador(num, QUANT - 1));
 }
